6c2807dee964e1be57ace074d2c487f0399e947b
kamaboko1 <github65@hotmail.com>
2019-04-17 18:40:12 -0700

    added scroll effect to quotes on home page

--- landing/js/script.js
+++ landing/js/script.js
@@ -1,3 +1,27 @@
 
 
- 
+ function quoteAppear() {
+     let quote = document.querySelector('.quote-disappear');
+     let distance = quote.getBoundingClientRect().top;
+    
+     let windowHeight = window.innerHeight / 1.2;
+
+     if(distance < windowHeight){
+        quote.classList.add('quote-appear')
+     }
+ }
+
+ function quoteAppear2() {
+    let quote = document.querySelector('.quote-disappear2');
+    let distance = quote.getBoundingClientRect().top;
+   
+    let windowHeight = window.innerHeight / 1.2;
+
+    if(distance < windowHeight){
+       quote.classList.add('quote-appear2')
+    }
+}
+
+window.addEventListener('scroll', quoteAppear);
+
+window.addEventListener('scroll', quoteAppear2);
